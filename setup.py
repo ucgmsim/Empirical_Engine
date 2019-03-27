f5e99213ababd5d68ce7783cb7794f47c17e21d8
claudio <claudio.schill@canterbury.ac.nz>
2019-03-27 13:32:39 +1300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Empirical_Engine",
+    version="1.0.0",
+    author="Quakecore",
+    description="Package for empirical calculations",
+    url="https://github.com/ucgmsim/Empirical_Engine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+)
