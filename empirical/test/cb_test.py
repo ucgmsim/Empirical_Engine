236cfe2d6a89c6f11e34733680d6816b71499427
ttgenius <melodyzyz@gmail.com>
2019-05-31 17:20:07 +1200

    added some plot testing for AS against wiki

--- empirical/test/cb_test.py
+++ empirical/test/cb_test.py
@@ -5,9 +5,16 @@ sys.path.append('../..')
 from empirical.util.empirical_factory import compute_gmm
 
 from empirical.util.classdef import Site, Fault, GMM
+
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+
 M = [4.0, 5.4, 7.8]
 RRUP = [10, 70, 200]
-IMS = ['CAV', 'AI']
+CB_IMS = ['CAV', 'AI']
+AS_IMS = ['Ds575','Ds595', 'Ds2080']
 site = Site()
 
 fault = Fault()
@@ -26,12 +33,32 @@ site.Rx = -1
 site.Rtvz = 50
 site.z2p5 = 0.9186718412435146
 
-for im in IMS:
-    print(im)
-    for rrup in RRUP:
-        site.Rrup = rrup
-        for mag in M:
-            fault.Mw = mag
-            results = compute_gmm(fault, site, GMM.CB_12, im)
+# for im in CB_IMS:
+#     print(im)
+#     for rrup in RRUP:
+#         site.Rrup = rrup
+#         for mag in M:
+#             fault.Mw = mag
+#             results = compute_gmm(fault, site, GMM.CB_12, im)
+#             print(results)
+#             print('\n')
+M2 = [5, 6.25, 7.5]
+d = {}
+for mag in M2:
+    fault.Mw = mag
+    d[mag] = {}
+    for im in AS_IMS:
+        print(im)
+        d[mag][im] = []
+        for rrup in RRUP:
+            site.Rrup = rrup
+            results = compute_gmm(fault, site, GMM.AS_16, im)
+            d[mag][im].append(results[0])
             print(results)
             print('\n')
+print(d)
+
+for k in AS_IMS:
+    plt.plot(RRUP, d[6.25][k])
+plt.legend(AS_IMS)
+plt.savefig('/home/melody/astest')
