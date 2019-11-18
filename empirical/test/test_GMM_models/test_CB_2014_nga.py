17860df28be0cc48257acd0a4f9a01dc044339cb
Viktor Polak <viktor.polak@canterbury.ac.nz>
2019-11-19 11:04:55 +1300

    only run python code for testing

--- empirical/test/test_GMM_models/test_CB_2014_nga.py
+++ empirical/test/test_GMM_models/test_CB_2014_nga.py
@@ -6,5 +6,5 @@ from subprocess import call
 matlab_tests = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_matlab")
 
 def test_CB_2014_nga():
-    call([os.path.join(matlab_tests, "CB_2014_nga_verify.m")])
-    call([os.path.join(matlab_tests, "CB_2014_nga_verify.py")])
+    #call([os.path.join(matlab_tests, "CB_2014_nga_verify.m")], cwd=matlab_tests)
+    call([os.path.join(matlab_tests, "CB_2014_nga_verify.py")], cwd=matlab_tests)
--- empirical/test/test_GMM_models/test_bc_hydro_2016_subduction.py
+++ empirical/test/test_GMM_models/test_bc_hydro_2016_subduction.py
@@ -6,5 +6,5 @@ from subprocess import call
 matlab_tests = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_matlab")
 
 def test_bc_hydro_2016_subduction():
-    call([os.path.join(matlab_tests, "bc_hydro_2016_subduction_verify.m")])
-    call([os.path.join(matlab_tests, "bc_hydro_2016_subduction_verify.py")])
+    #call([os.path.join(matlab_tests, "bc_hydro_2016_subduction_verify.m")], cwd=matlab_tests)
+    call([os.path.join(matlab_tests, "bc_hydro_2016_subduction_verify.py")], cwd=matlab_tests)
