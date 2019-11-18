058426f8b5ffb710eeb138f7707f8d08bc6b6acd
Viktor Polak <viktor.polak@canterbury.ac.nz>
2019-11-18 17:09:52 +1300

    made matlab testing into pytest, runs

---
+++ empirical/test/test_GMM_models/test_CB_2014_nga.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import os
+from subprocess import call
+
+matlab_tests = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_matlab")
+
+def test_CB_2014_nga():
+    call([os.path.join(matlab_tests, "CB_2014_nga_verify.m")])
+    call([os.path.join(matlab_tests, "CB_2014_nga_verify.py")])
