d2711a4b587d5f3d0c19d2a45886ecc7b1c45696
jpa198 <james.paterson@canterbury.ac.nz>
2019-12-06 16:16:42 +1300

    verification should be lower case in all places Add function to get the verification directory of a realisation Made calculate_unperturbated_empiricals work with the current 1/many realisation naming scheme

--- empirical/scripts/calculate_unperturbated_empiricals.py
+++ empirical/scripts/calculate_unperturbated_empiricals.py
@@ -2,22 +2,22 @@ import argparse
 import pathlib
 from multiprocessing.pool import Pool
 
-import yaml
 from qcore.formats import load_fault_selection_file
 from qcore.simulation_structure import (
     get_sources_dir,
-    get_fault_dir,
-    get_root_yaml_path,
-    get_sim_dir,
     get_realisation_name,
+    get_fault_from_realisation,
+    get_srf_info_location,
+    get_realisation_verification_dir,
 )
+from qcore.utils import load_yaml
 
 from empirical.scripts.calculate_empirical import IM_LIST, calculate_empirical
 from empirical.util import classdef
 
 
 def load_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser("""Script to generate """)
 
     def absolute_path(path):
         """Takes a path string and returns the absolute path object to it"""
@@ -63,21 +63,17 @@ def load_args():
 
 def create_event_tasks(events, sim_root, config_file, vs30_default, extended_period):
     tasks = []
-    for event_name in events:
-        fault_info = (
-            pathlib.Path(get_sources_dir(sim_root)).resolve()
-            / event_name
-            / f"{event_name}.info"
+    sources = pathlib.Path(get_sources_dir(sim_root))
+    vs30_file = load_yaml(pathlib.Path(sim_root) / "Runs" / "root_params.yaml")[
+        "stat_vs_est"
+    ]
+    for realisation_name in events:
+        event_name = get_fault_from_realisation(realisation_name)
+        fault_info = sources / get_srf_info_location(event_name)
+        output_dir = pathlib.Path(
+            get_realisation_verification_dir(sim_root, realisation_name)
         )
-        output_dir = pathlib.Path(get_fault_dir(sim_root, event_name)) / event_name
-        output_dir.mkdir()
-        vs30_file = yaml.safe_load(get_root_yaml_path(sim_root))["stat_vs_est"]
-        first_rel = get_realisation_name(event_name, 1)
-        rupture_distance = (
-            pathlib.Path(get_sim_dir(sim_root, first_rel))
-            / "verification"
-            / f"rrup_{first_rel}.csv"
-        )
+        rupture_distance = output_dir / f"rrup_{event_name}.csv"
         tasks.append(
             [
                 event_name,
@@ -99,7 +95,10 @@ def create_event_tasks(events, sim_root, config_file, vs30_default, extended_per
 def main():
     args = load_args()
 
-    events = load_fault_selection_file(args.fault_selection_file).keys()
+    events = load_fault_selection_file(args.fault_selection_file)
+    events = [
+        name if count == 1 else get_realisation_name(name, 1) for name, count in events
+    ]
     tasks = create_event_tasks(
         events,
         args.simulation_root,
