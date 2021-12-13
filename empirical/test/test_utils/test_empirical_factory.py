b4073d9da8c11655f13f0b577fbc164890d5709f
Jason <25143301+jasonmotha@users.noreply.github.com>
2021-12-14 09:48:17 +1300

    passing tests

--- empirical/test/test_utils/test_empirical_factory.py
+++ empirical/test/test_utils/test_empirical_factory.py
@@ -4,16 +4,17 @@ from empirical.util import empirical_factory
 from empirical.util.classdef import SiteClass
 
 
-VS30S = [100, 200, 300, 600, 1100]
+VS30S = [100, 201, 301, 601, 1101]
 EXPECTED_RESULTS = [
     (100, SiteClass.SOFTSOIL),
-    (200, SiteClass.MEDIUMSOIL),
-    (300, SiteClass.HARDSOIL),
-    (600, SiteClass.ROCK),
-    (1100, SiteClass.HARDROCK),
+    (201, SiteClass.MEDIUMSOIL),
+    (301, SiteClass.HARDSOIL),
+    (601, SiteClass.ROCK),
+    (1101, SiteClass.HARDROCK),
 ]
 
 
 @pytest.mark.parametrize("test_vs30, expected_siteclass", EXPECTED_RESULTS)
 def test_determine_siteclass(test_vs30, expected_siteclass):
+    print(test_vs30)
     assert empirical_factory.determine_siteclass(test_vs30) == expected_siteclass
