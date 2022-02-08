1ec107da58c40fe396d46621a608a380ff9b6eb3
sdw123123@gmail.com <sdw123123@gmail.com>
2022-02-08 16:50:08 +1300

    using loc to keep the consistency

--- empirical/util/openquake_wrapper_vectorized.py
+++ empirical/util/openquake_wrapper_vectorized.py
@@ -41,76 +41,6 @@ CTX_PROPERTIES = [
     ("rvolc", "Rtvz"),
 ]
 
-OQ_GMM_LIST = [
-    GMM.P_20,
-    GMM.HA_20,
-    GMM.G_17,
-    GMM.BC_16,
-    GMM.S_16,
-    GMM.Ph_20,
-    GMM.Ch_20,
-    GMM.AG_20,
-    GMM.AG_20_NZ,
-    GMM.K_20,
-    GMM.K_20_NZ,
-    GMM.Si_20,
-    GMM.Z_16,
-    GMM.Br_13,
-]
-if OQ:
-    oq_models = {
-        GMM.P_20: {
-            TectType.SUBDUCTION_SLAB: gsim.parker_2020.ParkerEtAl2020SSlab,
-            TectType.SUBDUCTION_INTERFACE: gsim.parker_2020.ParkerEtAl2020SInter,
-        },
-        GMM.HA_20: {
-            TectType.ACTIVE_SHALLOW: gsim.hassani_atkinson_2020.HassaniAtkinson2020Asc,
-            TectType.SUBDUCTION_SLAB: gsim.hassani_atkinson_2020.HassaniAtkinson2020SSlab,
-            TectType.SUBDUCTION_INTERFACE: gsim.hassani_atkinson_2020.HassaniAtkinson2020SInter,
-        },
-        GMM.G_17: {TectType.ACTIVE_SHALLOW: gsim.gulerce_2017.GulerceEtAl2017},
-        GMM.BC_16: {
-            TectType.ACTIVE_SHALLOW: gsim.bozorgnia_campbell_2016.BozorgniaCampbell2016
-        },
-        GMM.S_16: {TectType.ACTIVE_SHALLOW: gsim.stewart_2016_vh.StewartEtAl2016VH},
-        GMM.Ph_20: {
-            TectType.ACTIVE_SHALLOW: gsim.phung_2020.PhungEtAl2020Asc,
-            TectType.SUBDUCTION_SLAB: gsim.phung_2020.PhungEtAl2020SSlab,
-            TectType.SUBDUCTION_INTERFACE: gsim.phung_2020.PhungEtAl2020SInter,
-        },
-        GMM.Ch_20: {
-            TectType.ACTIVE_SHALLOW: gsim.chao_2020.ChaoEtAl2020Asc,
-            TectType.SUBDUCTION_SLAB: gsim.chao_2020.ChaoEtAl2020SSlab,
-            TectType.SUBDUCTION_INTERFACE: gsim.chao_2020.ChaoEtAl2020SInter,
-        },
-        GMM.AG_20: {
-            TectType.SUBDUCTION_SLAB: gsim.abrahamson_gulerce_2020.AbrahamsonGulerce2020SSlab,
-            TectType.SUBDUCTION_INTERFACE: gsim.abrahamson_gulerce_2020.AbrahamsonGulerce2020SInter,
-        },
-        GMM.AG_20_NZ: {
-            TectType.SUBDUCTION_SLAB: gsim.abrahamson_gulerce_2020.AbrahamsonGulerce2020SSlab,
-            TectType.SUBDUCTION_INTERFACE: gsim.abrahamson_gulerce_2020.AbrahamsonGulerce2020SInter,
-        },
-        GMM.K_20: {
-            TectType.SUBDUCTION_SLAB: gsim.kuehn_2020.KuehnEtAl2020SSlab,
-            TectType.SUBDUCTION_INTERFACE: gsim.kuehn_2020.KuehnEtAl2020SInter,
-        },
-        GMM.K_20_NZ: {
-            TectType.SUBDUCTION_SLAB: gsim.kuehn_2020.KuehnEtAl2020SSlab,
-            TectType.SUBDUCTION_INTERFACE: gsim.kuehn_2020.KuehnEtAl2020SInter,
-        },
-        GMM.Si_20: {
-            TectType.SUBDUCTION_SLAB: gsim.si_2020.SiEtAl2020SSlab,
-            TectType.SUBDUCTION_INTERFACE: gsim.si_2020.SiEtAl2020SInter,
-        },
-        GMM.Z_16: {
-            TectType.ACTIVE_SHALLOW: gsim.zhao_2016.ZhaoEtAl2016Asc,
-            TectType.SUBDUCTION_SLAB: gsim.zhao_2016.ZhaoEtAl2016SSlab,
-            TectType.SUBDUCTION_INTERFACE: gsim.zhao_2016.ZhaoEtAl2016SInter,
-        },
-        GMM.Br_13: {TectType.ACTIVE_SHALLOW: gsim.bradley_2013.Bradley2013},
-    }
-
 
 def oq_mean_stddevs(model, ctx, imr, stddev_types):
     """
@@ -175,21 +105,21 @@ def oq_run(model, rupture_df, im, period=None, **kwargs):
             # Sites
             # Create a dummy location as OQ calculation doesn't use a location
             ("location", Point(0.0, 0.0, 0.0)),
-            ("vs30", rupture_df.vs30.values),
-            ("z1pt0", rupture_df.z1pt0.values),
-            ("z2pt5", rupture_df.z2pt5.values),
-            ("vs30measured", rupture_df.vs30measured.values),
+            ("vs30", rupture_df.loc[:, "vs30"].values),
+            ("z1pt0", rupture_df.loc[:, "z1pt0"].values),
+            ("z2pt5", rupture_df.loc[:, "z2pt5"].values),
+            ("vs30measured", rupture_df.loc[:, "vs30measured"].values),
             # Distances
-            ("rrup", rupture_df.rrup.values),
-            ("rjb", rupture_df.rjb.values),
-            ("rx", rupture_df.rx.values),
-            ("rvolc", rupture_df.rvolc.values),
+            ("rrup", rupture_df.loc[:, "rrup"].values),
+            ("rjb", rupture_df.loc[:, "rjb"].values),
+            ("rx", rupture_df.loc[:, "rx"].values),
+            ("rvolc", rupture_df.loc[:, "rvolc"].values),
             # Rupture
-            ("mag", rupture_df.mag.values),
-            ("rake", rupture_df.rake.values),
-            ("dip", rupture_df.dip.values),
-            ("ztor", rupture_df.ztor.values),
-            ("hypo_depth", rupture_df.hypo_depth.values),
+            ("mag", rupture_df.loc[:, "mag"].values),
+            ("rake", rupture_df.loc[:, "rake"].values),
+            ("dip", rupture_df.loc[:, "dip"].values),
+            ("ztor", rupture_df.loc[:, "ztor"].values),
+            ("hypo_depth", rupture_df.loc[:, "hypo_depth"].values),
             # Openquake requiring occurrence_rate attribute to exist
             ("occurrence_rate", None),
         )
@@ -210,7 +140,9 @@ def oq_run(model, rupture_df, im, period=None, **kwargs):
             # interpolate pSA value up based on maximum available period
             if p > max_period:
                 result.update(
-                    pd.Series(result.get("mean") * (max_period / p) ** 2, name="mean")
+                    pd.Series(
+                        result.loc[:, "mean"] * (max_period / p) ** 2, name="mean"
+                    )
                 )
             results.append(result)
         if single:
