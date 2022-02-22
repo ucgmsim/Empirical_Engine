cd7d0fe63b7ede51f450af649365cb19d17d1f9f
sdw123123@gmail.com <sdw123123@gmail.com>
2022-02-23 11:17:37 +1300

    Update PR comments

--- empirical/util/openquake_wrapper_vectorized.py
+++ empirical/util/openquake_wrapper_vectorized.py
@@ -1,5 +1,5 @@
 """Wrapper for openquake vectorized models."""
-from typing import List
+from typing import Sequence
 
 import pandas as pd
 
@@ -20,27 +20,33 @@ OQ_MODELS = {
     "AG_20_NZ": gsim.kuehn_2020.KuehnEtAl2020SInter,
 }
 
+SPT_STD_DEVS = [const.StdDev.TOTAL, const.StdDev.INTER_EVENT, const.StdDev.INTRA_EVENT]
 
-def convert_im_label(im: imt):
+
+def convert_im_label(im: imt.IMT):
     """Convert OQ's SA(period) to the internal naming, pSA_period
-    im: imt
+    im: imt.IMT
     """
     imt_tuple = imt.imt2tup(im.string)
     return im if len(imt_tuple) == 1 else f"pSA_{imt_tuple[-1]}"
 
 
 def oq_mean_stddevs(
-    model: gsim, ctx: contexts.RuptureContext, im: imt, stddev_types: List
+    model: gsim.base.GMPE,
+    ctx: contexts.RuptureContext,
+    im: imt.IMT,
+    stddev_types: Sequence[const.StdDev],
 ):
     """Calculate mean and standard deviations given openquake input structures.
-    model: OQ Model
+    model: gsim.base.GMPE
+        OQ models we use are subclass of gsim.base.GMPE
     ctx: contexts.RuptureContext
         OQ RuptureContext that contains the following information
         - Site
         - Distance
         - Rupture
-    im: imt
-    stddev_types: List
+    im: imt.IMT
+    stddev_types: Sequence[const.StdDev]
     """
     mean, stddevs = model.get_mean_and_stddevs(ctx, ctx, ctx, im, stddev_types)
     mean_stddev_dict = {f"{convert_im_label(im)}_mean": mean}
@@ -53,19 +59,25 @@ def oq_mean_stddevs(
 
 
 def oq_run(
-    model: str, rupture_df: pd.DataFrame, im: str, period: List = None, **kwargs
+    model: str,
+    rupture_df: pd.DataFrame,
+    im: str,
+    period: Sequence[int] = None,
+    **kwargs,
 ):
     """Run an openquake model with dataframe
-    model: OQ model name, string
-        Only support Bradley_2013 for now
+    model: string
+        OQ model name
     rupture_df: Rupture DF
         Columns for properties. E.g., vs30, z1pt0, rrup, rjb, mag, rake, dip....
         Rows be the separate site-fault pairs
         But Site information must be identical across the rows,
         only the faults can be different.
-    im: intensity measure name
-    period: for spectral acceleration, openquake tables automatically
-            interpolate values between specified values, fails if outside range
+    im: string
+        intensity measure
+    period: Sequence[int]
+        for spectral acceleration, openquake tables automatically
+        interpolate values between specified values, fails if outside range
     kwargs: pass extra (model specific) parameters to models
     """
     model = (
@@ -74,13 +86,15 @@ def oq_run(
         else OQ_MODELS[model](region="NZL", **kwargs)
     )
 
-    stddev_types = []
-    for st in [const.StdDev.TOTAL, const.StdDev.INTER_EVENT, const.StdDev.INTRA_EVENT]:
-        if st in model.DEFINED_FOR_STANDARD_DEVIATION_TYPES:
-            stddev_types.append(st)
+    stddev_types = [
+        std for std in SPT_STD_DEVS if std in model.DEFINED_FOR_STANDARD_DEVIATION_TYPES
+    ]
+
+    # Make a copy in case the original rupture_df used with other functions
+    rupture_df = rupture_df.copy()
 
     # Check if df contains what model requires
-    rupture_ctx_properties = list(rupture_df.columns.values)
+    rupture_ctx_properties = set(rupture_df.columns.values)
     extra_site_parameters = set(model.REQUIRES_SITES_PARAMETERS).difference(
         rupture_ctx_properties
     )
@@ -101,22 +115,20 @@ def oq_run(
             "unknown distance property: " + " ".join(extra_dist_properties)
         )
 
-    # Make a copy in case the original rupture_df used with other functions
-    copied_rupture_df = rupture_df.copy()
     # Convert z1pt0 from km to m
-    copied_rupture_df["z1pt0"] *= 1000
+    rupture_df["z1pt0"] *= 1000
     # OQ's single new-style context which contains all site, distance and rupture's information
     rupture_ctx = contexts.RuptureContext(
         tuple(
             [
                 # Openquake requiring occurrence_rate attribute to exist
                 ("occurrence_rate", None),
-                # sids is basically the number of sites provided
-                # each row of DF is site & rupture pair
-                ("sids", [0] * len(rupture_df.index)),
+                # sids is the number of sites provided (OQ term)
+                # This term needs to be repeated for the number of rows in the df
+                ("sids", [1] * rupture_df.shape[0]),
                 *(
-                    (column, copied_rupture_df.loc[:, column].values,)
-                    for column in copied_rupture_df.columns.values
+                    (column, rupture_df.loc[:, column].values,)
+                    for column in rupture_df.columns.values
                 ),
             ]
         )
