d890774a084fa7f9b5f6e67061d96f309d19ce7f
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2026-01-19 14:30:08 +1300

    fix: add back the DataFrame bound

--- oq_wrapper/types.py
+++ oq_wrapper/types.py
@@ -4,4 +4,4 @@ import numpy as np
 import numpy.typing as npt
 import pandas as pd
 
-Array = float | npt.NDArray[np.floating] | pd.Series
+Array = float | npt.NDArray[np.floating] | pd.Series | pd.DataFrame
