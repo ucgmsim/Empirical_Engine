3f0e1136b644660572868f2b7ecf15213f681d93
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2026-01-23 14:00:05 +1300

    tests(wrapper): regression test z1pt0 change

---
+++ tests/test_wrapper.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import pytest
+
+import oq_wrapper as oqw
+
+
+def test_run_gmm_z1pt0_handling() -> None:
+    rupture_df = pd.DataFrame(
+        {
+            "mag": [6.5],
+            "vs30": 400.0,
+            "rrup": 100.0,
+        }
+    )
+
+    # Result runs without throwing
+    _ = oqw.run_gmm(
+        oqw.constants.GMM.A_22, oqw.constants.TectType.ACTIVE_SHALLOW, rupture_df, "PGA"
+    )
+
+    rupture_df = pd.DataFrame(
+        {"mag": [6.5], "vs30": 400.0, "rrup": 100.0, "z1pt0": 1000.0}
+    )
+
+    # Result fails because of z1pt0 error
+    with pytest.raises(ValueError, match=".*Z1.0 values.*"):
+        _ = oqw.run_gmm(
+            oqw.constants.GMM.A_22,
+            oqw.constants.TectType.ACTIVE_SHALLOW,
+            rupture_df,
+            "PGA",
+        )
